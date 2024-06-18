b3ec64c749ba0a0907bc431b74d7f25bd888b86c
lisandroviedo <lisandroviedo@hotmail.com>
2024-06-18 00:18:20 -0400

    Añadidas rutas de imágenes

--- Server/src/utils/equipos.js
+++ Server/src/utils/equipos.js
@@ -1,20 +1,68 @@
 const equipos = [
-  "Argentina",
-  "Perú",
-  "Chile",
-  "Canadá",
-  "México",
-  "Ecuador",
-  "Venezuela",
-  "Jamaica",
-  "Estados Unidos",
-  "Uruguay",
-  "Panamá",
-  "Bolivia",
-  "Brasil",
-  "Colombia",
-  "Paraguay",
-  "Costa Rica",
+  {
+    nombre: "Argentina",
+    ruta_imagen: "img/Argentina.svg",
+  },
+  {
+    nombre: "Perú",
+    ruta_imagen: "img/Peru.svg",
+  },
+  {
+    nombre: "Chile",
+    ruta_imagen: "img/Chile.svg",
+  },
+  {
+    nombre: "Canada",
+    ruta_imagen: "img/Canada.svg",
+  },
+  {
+    nombre: "México",
+    ruta_imagen: "img/Mexico.svg",
+  },
+  {
+    nombre: "Ecuador",
+    ruta_imagen: "img/Ecuador.svg",
+  },
+  {
+    nombre: "Venezuela",
+    ruta_imagen: "img/Venezuela.svg",
+  },
+  {
+    nombre: "Jamaica",
+    ruta_imagen: "img/Jamaica.svg",
+  },
+  {
+    nombre: "Estados Unidos",
+    ruta_imagen: "img/EstadosUnidos.svg",
+  },
+  {
+    nombre: "Uruguay",
+    ruta_imagen: "img/Uruguay.svg",
+  },
+  {
+    nombre: "Panamá",
+    ruta_imagen: "img/Panama.svg",
+  },
+  {
+    nombre: "Bolivia",
+    ruta_imagen: "img/Bolivia.svg",
+  },
+  {
+    nombre: "Brasil",
+    ruta_imagen: "img/Brasil.svg",
+  },
+  {
+    nombre: "Colombia",
+    ruta_imagen: "img/Colombia.svg",
+  },
+  {
+    nombre: "Paraguay",
+    ruta_imagen: "img/Paraguay.svg",
+  },
+  {
+    nombre: "Costa Rica",
+    ruta_imagen: "img/CostaRica.svg",
+  },
 ];
 
 module.exports = {
