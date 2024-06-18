5c3b5d6605f9a4bde4a090055e152c475989a55b
lisandroviedo <lisandroviedo@hotmail.com>
2024-06-18 00:17:05 -0400

    Inhabilitación de funciones

--- Server/index.js
+++ Server/index.js
@@ -4,31 +4,31 @@ const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT_SERVER || 3001;
 
-// const { cargarRoles } = require("./src/controllers/roles_controllers.js");
-// const { cargarEmpresas } = require("./src/controllers/empresas_controllers.js");
-// const {
-//   cargarEmpleados,
-// } = require("./src/controllers/empleados_controllers.js");
-// const { cargarEquipos } = require("./src/controllers/equipos_controllers.js");
-// const { cargarTorneos } = require("./src/controllers/torneos_controllers.js");
-// const { cargarTorneos } = require("./src/controllers/torneos_controllers.js");
-// const { cargarPartidos } = require("./src/controllers/partidos_controllers.js");
+const { cargarRoles } = require("./src/controllers/roles_controllers.js");
+const { cargarEmpresas } = require("./src/controllers/empresas_controllers.js");
+const {
+  cargarEmpleados,
+} = require("./src/controllers/empleados_controllers.js");
+const { cargarEquipos } = require("./src/controllers/equipos_controllers.js");
+const { cargarTorneos } = require("./src/controllers/torneos_controllers.js");
+const { cargarPartidos } = require("./src/controllers/partidos_controllers.js");
+const {
+  cargarQuinielas,
+} = require("./src/controllers/quinielas_controllers.js");
 
 conn
-  .sync({ alter: true })
+  .sync({})
   .then(() => {
-    server.listen(PORT, () => {
+    server.listen(PORT, async () => {
       console.log(`Server listening on port ${PORT}`);
 
-      // cargarRoles();
-
-      // cargarEmpresas().then(() => {
-      // cargarEmpleados();
-      // });
-
-      // cargarEquipos();
-      // cargarTorneos();
-      // cargarPartidos();
+      // await cargarRoles();
+      // await cargarQuinielas();
+      // await cargarEmpresas();
+      // await cargarEmpleados();
+      // await cargarEquipos();
+      // await cargarTorneos();
+      // await cargarPartidos();
     });
   })
   .catch((error) => console.error(error));
