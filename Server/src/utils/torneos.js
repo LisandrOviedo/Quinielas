eabb797f8097143615177c9066a761f866994f69
lisandroviedo <lisandroviedo@hotmail.com>
2024-06-18 00:16:48 -0400

    Corrección de error

--- Server/src/utils/torneos.js
+++ Server/src/utils/torneos.js
@@ -1,8 +1,8 @@
 const torneos = [
   {
     nombre: "Copa América 2024",
-    fecha_inicio: Date("2024/06/20"),
-    fecha_fin: Date("2024/07/14"),
+    fecha_inicio: "2024/06/20",
+    fecha_fin: "2024/07/14",
   },
 ];
 
