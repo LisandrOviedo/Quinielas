343d813b8160e3ea63080b078ccaec51c8906a9e
LisandrOviedo <lisandroviedo@hotmail.com>
2024-06-20 17:03:59 -0400

    Añadidas 2 nuevas empresas
    Eliminadas líneas de código innecesarias

--- Server/src/utils/empresas.js
+++ Server/src/utils/empresas.js
@@ -3,6 +3,14 @@ const empresas_faltantes = [
     quiniela: "Maracaibo",
     nombre: "Corporativo",
   },
+  {
+    quiniela: "Maracaibo",
+    nombre: "Los Claros Gastronomía",
+  },
+  {
+    quiniela: "Maracaibo",
+    nombre: "Motores Lago",
+  },
 ];
 
 module.exports = {
