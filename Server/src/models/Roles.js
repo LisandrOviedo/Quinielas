91196b91ea816044197ac7ccd70f7d27df745e72
LisandrOviedo <lisandroviedo@hotmail.com>
2024-07-17 15:14:58 -0400

    Removido index innecesario

--- Server/src/models/Roles.js
+++ Server/src/models/Roles.js
@@ -1,35 +1,23 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Roles",
-    {
-      rol_id: {
-        type: DataTypes.INTEGER,
+  sequelize.define("Roles", {
+    rol_id: {
+      type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
-      },
-      nombre: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      descripcion: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      activo: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-      },
     },
-    {
-      indexes: [
-        {
-          unique: true,
-          name: "nombre",
-          fields: ["nombre"],
-        },
-      ],
-    }
-  );
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    activo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  });
 };
